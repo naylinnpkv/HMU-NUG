17e091ee604f7697a3949c887e528d98ebdd679d
Nay Linn <61095225+naylinnpkv@users.noreply.github.com>
2022-07-20 20:23:12 -0400

    all basic functionality done

--- src/views/Ticket.tsx
+++ src/views/Ticket.tsx
@@ -1,15 +1,25 @@
 import Raffle from "../statics/raffle.png";
 import html2canvas from "html2canvas";
+import axios from "axios";
 import { Button } from "antd";
 import styles from "../statics/_ticket.css";
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { TicketInputs } from "./TicketInputs";
+import _ from "lodash";
+import { ITicketInput } from "../models";
+
 export const Ticket = () => {
   const [ticketNumber, setTicketNumber] = useState<string>("");
   const [name, setName] = useState<string>("");
   const [country, setCountry] = useState<string>("");
   const [contact, setContact] = useState<string>("");
+  const [agentName, setAgentName] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(false);
+
   const printRef = useRef<any>();
+
+  const sheetUrl =
+    "https://sheet.best/api/sheets/6bdca4b6-2a5c-4f26-bbe5-22dff9091666";
 
   const handleDownloadImage = async () => {
     const element = printRef.current;
@@ -30,6 +40,38 @@ export const Ticket = () => {
     }
   };
 
+  const getData = async () => {
+    setLoading(true);
+    const { data } = await axios.get<ITicketInput[]>(sheetUrl);
+
+    if (data.length === 0) {
+      setTicketNumber(_.toString(1).padStart(5, "0"));
+      setLoading(false);
+      return;
+    }
+    setTicketNumber(_.toString(data.length + 1).padStart(5, "0"));
+    setLoading(false);
+    return;
+  };
+
+  useEffect(() => {
+    getData();
+  }, [ticketNumber]);
+
+  const postData = async () => {
+    setLoading(true);
+    const payLoad = { ticketNumber, name, country, contact, agentName };
+
+    const { data } = await axios.post<ITicketInput[]>(sheetUrl, payLoad);
+
+    setTicketNumber(_.toString(data.length + 1).padStart(5, "0"));
+    setName("");
+    setContact("");
+    setAgentName("");
+    setCountry("");
+    setLoading(false);
+  };
+
   return (
     <>
       <div className="head-image" style={{ maxWidth: "768px" }} ref={printRef}>
@@ -46,18 +88,28 @@ export const Ticket = () => {
       <div className="formWrapper">
         <TicketInputs
           setTicketNumber={setTicketNumber}
+          ticketNumber={ticketNumber}
           setName={setName}
+          name={name}
           setCountry={setCountry}
+          country={country}
           setContact={setContact}
+          contact={contact}
+          setAgentName={setAgentName}
+          agentName={agentName}
         />
         <div className="ticket-input">
           <div className="download-button">
             <Button
-              onClick={handleDownloadImage}
+              onClick={() => {
+                handleDownloadImage();
+                postData();
+              }}
               type="primary"
               size="small"
               shape="round"
               style={{ minWidth: "150px" }}
+              loading={loading}
             >
               Download Ticket in JPEG
             </Button>
