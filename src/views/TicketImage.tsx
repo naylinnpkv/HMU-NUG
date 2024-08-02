ea2d15238162cbed46cb4cb3afeb5937aac323d0
Nay Linn <naylinn@nays-mbp.mynetworksettings.com>
2024-08-02 17:05:45 -0400

    basic features are done

--- src/views/TicketImage.tsx
+++ src/views/TicketImage.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Raffle from "../statics/2023_Deepavali_ticket.png";
 import JapanRaffle from "../statics/2023_Japan.png";
 import Chin_2024 from "../statics/2024_chin_ticket.jpg";
+import Diwali_2024 from "../statics/2024_MHU_Diwali.jpg";
 
 export const TicketImage: React.FC<{
   isMultiple: boolean;
@@ -27,15 +28,13 @@ export const TicketImage: React.FC<{
   return (
     <div className="head-image" style={{ maxWidth: "768px" }} ref={printRef}>
       <p className="in-the-ticket-region">{onImageTicketNum}</p>
-      <img src={Chin_2024} alt="Raffle" className="ticket" />
+      <img src={Diwali_2024} alt="Raffle" className="ticket" />
       <div className="text-on-image">
         <div className="text-on-image-innerwrapper">
+          <p>{onImageTicketNum}</p>
           <p> {`${name}`}</p>
           <p className="country_on_img"> {`${country}`}</p>
           <p className="contact_on_img"> {`${contact}`}</p>
-          <p style={{ marginTop: "19.5px" }} className="main_ticket_on_img">
-            {onImageTicketNum}
-          </p>
         </div>
       </div>
     </div>
